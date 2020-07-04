405bddf41a764286d5aecc9333a20734888385d9
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2020-07-03 23:07:17 -0700

    create js game plan

---
+++ index.js
@@ -0,0 +1,12 @@
+/* 
+  Development Notes:
+    - Consider front loading some of the computation on page load 
+
+  TODOs:
+    [] Create first ball drop
+      [] Start with simple drop from center and straight down
+      [] Generate origin, trajectory, and spin
+      [] Create ball with complimentary color gradient
+      [] Launch ball with generated variables
+*/
+
