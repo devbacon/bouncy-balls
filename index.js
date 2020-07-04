21f98c4fbfab70038ab38dd58662be3ef1e44511
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2020-07-03 23:09:03 -0700

    add color hex references

--- index.js
+++ index.js
@@ -10,3 +10,17 @@
       [] Launch ball with generated variables
 */
 
+const colorHex = {
+  red: '#F00',
+  cyan: '#0FF',
+  green: '#0F0',
+  magenta: '#F0F',
+  blue: '#00F',
+  yellow: 'FF0'
+};
+
+const complementaryColors = [
+  [colorHex.red, colorHex.cyan],
+  [colorHex.green, colorHex.magenta],
+  [colorHex.blue, colorHex.yellow]
+];
