d8deefaa08c453e59e223de3224e4f2335dcc864
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2020-07-04 20:47:02 -0700

    clean code and expand class properties

--- index.js
+++ index.js
@@ -2,15 +2,9 @@
   Development Notes:
     - Consider front loading some of the computation on page load
     - Control CSS animations and transitions with JS
-
-  TODOs:
-    [] Create first ball drop
-      [] Start with simple drop from center and straight down
-      [] Generate origin, trajectory, and spin
-      [] Create ball with complimentary color gradient
-      [] Launch ball with generated variables
 */
 
+/* TODO: Move colors to CSS */
 const colorHex = {
   red: '#F00',
   cyan: '#0FF',
@@ -39,7 +33,7 @@ const boxAngles = {
 };
 
 class Ball {
-  constructor(boundAreaEl, startX, startY, direction, speed) {
+  constructor(startX, startY, direction, speed) {
     /* Randomize colors */
     const colorPairIndex = Math.floor(Math.random() * complementaryColors.length);
     const colorPair = complementaryColors[colorPairIndex];
@@ -48,24 +42,35 @@ class Ball {
 
     /* Generate ball DOM element with selected colors */
     const element = document.createElement('div');
+    element.addEventListener('click', this.leaveDOM.bind(this));
     element.style.background = `linear-gradient(35deg, ${this.primaryColor}, ${this.secondaryColor})`;
     element.classList.add('ball');
-
-    /* Static properties */
-    this.boundAreaEl = boundAreaEl;
-    this.element = element;
     
     /* Dynamic properties */
-    this.boundAreaStyles = window.getComputedStyle(this.boundAreaEl);
-    this.boundAreaHeight = this.boundAreaStyles.getPropertyValue('height');
-    this.boundAreaWidth = this.boundAreaStyles.getPropertyValue('width');
+    this.boundAreaEl = null;
+    this.boundAreaStyles = null;
+    this.boundAreaHeight = null;
+    this.boundAreaWidth = null;
+    this.element = element;
     this.styles = null;
     this.circumference = null;
     this.radius = null;
     this.direction = direction;
     this.speed = speed;
+    /* Coordinate plane starts at top left of bound area */
     this.x = startX;
     this.y = startY;
+    this.spaceTop = null;
+    this.spaceRight = null;
+    this.spaceBottom = null;
+    this.spaceLeft = null;
+    this.nextWallCollision = null;
+  }
+
+  enterDOM(container) {
+    this.boundAreaEl = container;
+    container.appendChild(this.element);
+    this.updatePropData();
   }
 
   updatePropData() {
@@ -77,25 +82,11 @@ class Ball {
     this.radius = this.circumference / 2;
     this.x = this.element.offsetLeft;
     this.y = this.element.offsetTop;
-  }
-
-  enterDOM() {
-
-  }
-
-  /* Travel along path based on direction and velocity */
-  startTransition() {
-    /* 
-      Steps:
-        1. Figure out which wall will be hit
-        2. Check the distance between ball edge and wall, factoring in direction
-          - Literal edge case: if ball is heading for a corner at a 45deg angle
-        3. Determine how long it will take to reach wall using distance and speed
-        4. Set CSS transition based on gathered parameters
-          - Set transition-origin to the wall edge direction
-    */
-
-    
+    this.spaceTop = this.y - this.radius;
+    this.spaceRight = this.boundAreaWidth - this.x - this.radius;
+    this.spaceBottom = this.boundAreaHeight - this.y - this.radius;
+    this.spaceLeft = this.x - this.radius;
+    this.nextWallCollision = this.findNextWallCollision();
   }
 
   findNextWallCollision() {
@@ -114,6 +105,42 @@ class Ball {
     return 'error';
   }
 
+  /* Travel along path based on direction and velocity */
+  startTransition() {
+    /* 
+      Steps:
+        1. Figure out which wall will be hit
+        2. Check the distance between ball edge and wall, factoring in direction
+          - Literal edge case: if ball is heading for a corner at a 45deg angle
+        3. Determine how long it will take to reach wall using distance and speed
+        4. Set CSS transition based on gathered parameters
+          - Set transition-origin to the wall edge direction
+    */
+
+    this.updatePropData();
+    this.element.style.transitionOrigin = nextWallCollision;
+
+    /* Determine distance to wall, travel time, and landing coordinates */
+    switch (nextWallCollision) {
+      case 'top':
+        
+        break;
+      case 'right':
+
+        break;
+      case 'bottom':
+
+        break;
+      case 'left':
+
+        break;
+      default:
+        break;
+    }
+    
+    this.element.style.transitionDuration = distanceToWall / this.speed;
+  }
+
   findHypotenuseBySides(length1, length2) {
     return Math.sqrt(Math.pow(length1, 2) + Math.pow(length2, 2));
   }
@@ -122,16 +149,17 @@ class Ball {
     
   }
 
-  leaveDOM() {
-
+  leaveDOM(event) {
+    event.stopPropagation();
+    this.boundAreaEl.removeChild(this.element);
   }
 }
 
 /* Set click listener bounding area to spawn in bouncy */
 const boundAreaEl = document.querySelector('#ball-bounding-area');
-boundAreaEl.addEventListener('click', spawnBall);
+boundAreaEl.addEventListener('click', addBall);
 
-function spawnBall() {
-  const ball = new Ball(boundAreaEl, 0, 0, 150, 10);
-  boundAreaEl.appendChild(ball.element);
+function addBall() {
+  const ball = new Ball(0, 0, 150, 10);
+  ball.enterDOM(boundAreaEl);
 }
