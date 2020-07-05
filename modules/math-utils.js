095e352b43670d25d3063e85ed85e0c4d6d7318d
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2020-07-04 21:28:16 -0700

    create math utils module with right angle methods

---
+++ modules/math-utils.js
@@ -0,0 +1,19 @@
+/* Custom math utility short cuts exported as MathUtils object */
+
+export default MathUtils = {
+  /* Determine trigonometry operation based on missing side */
+  findRightTriangleOppositeLength(angle, adjacentLength, hypotenuseLength) {
+
+  },
+  findRightTriangleAdjacentLength(angle, oppositeLength, hypotenuseLength) {
+
+  },
+  findRightTriangleHypotenuseLength(angle, oppositeLength, adjacentLength) {
+
+  },
+  getRightTriangleDimensions(angle1, sideLength1, angle2, sideLength2, angle3, sideLength3) {
+    /* Return all obtainable info using known sides and angles */
+    
+  },
+}
+
