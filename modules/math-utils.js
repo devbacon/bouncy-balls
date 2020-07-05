a7cd0e08350810efef077e0475269fb4857c9b3c
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2020-07-04 21:54:32 -0700

    validate method inputs

--- modules/math-utils.js
+++ modules/math-utils.js
@@ -3,17 +3,62 @@
 export default MathUtils = {
   /* Determine trigonometry operation based on missing side */
   findRightTriangleOppositeLength(angle, adjacentLength, hypotenuseLength) {
+    /* Ensure that inputs are valid */
+    if(typeof angle != 'number' || 0 > angle > 180) return null;
+    const parsedAdjacentLength = parseInt(adjacentLength, 2);
+    const parsedHypotenuseLength = parseInt(hypotenuseLength, 2);
 
+    if (parsedAdjacentLength) {
+      /* Tangent */
+
+      return
+    }
+
+    if (parsedHypotenuseLength) {
+      /* Sine */
+
+      return
+    }
   },
   findRightTriangleAdjacentLength(angle, oppositeLength, hypotenuseLength) {
+    /* Ensure that inputs are valid */
+    if(typeof angle != 'number' || 0 > angle > 180) return null;
+    const parsedOppositeLength = parseInt(oppositeLength, 2);
+    const parsedHypotenuseLength = parseInt(hypotenuseLength, 2);
 
+    if (parsedOppositeLength) {
+      /* Tangent */
+      
+      return
+    }
+
+    if (parsedHypotenuseLength) {
+      /* Sine */
+
+      return
+    }
   },
   findRightTriangleHypotenuseLength(angle, oppositeLength, adjacentLength) {
+    /* Ensure that inputs are valid */
+    if(typeof angle != 'number' || 0 > angle > 180) return null;
+    const parsedOppositeLength = parseInt(oppositeLength, 2);
+    const parsedAdjacentLength = parseInt(adjacentLength, 2);
 
+    if (parsedOppositeLength) {
+      /* Tangent */
+      
+      return
+    }
+
+    if (parsedAdjacentLength) {
+      /* Sine */
+
+      return
+    }
   },
   getRightTriangleDimensions(angle1, sideLength1, angle2, sideLength2, angle3, sideLength3) {
     /* Return all obtainable info using known sides and angles */
-    
+
   },
 }
 
