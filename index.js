62ba914533811f8ee4c19db8e9934d1e803f723b
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2020-07-04 19:49:29 -0700

    fill Ball class constructor

--- index.js
+++ index.js
@@ -38,17 +38,32 @@ const boxAngles = {
 };
 
 class Ball {
-  constructor() {
-    // Compute all randomized variables upon creation
+  constructor(boundAreaEl, startX, startY, direction, speed) {
+    /* Randomize colors */
     const colorPairIndex = Math.floor(Math.random() * complementaryColors.length);
     const colorPair = complementaryColors[colorPairIndex];
-    const primaryColor = colorPair[0];
-    const secondaryColor = colorPair[1];
+    this.primaryColor = colorPair[0];
+    this.secondaryColor = colorPair[1];
+
+    /* Generate ball DOM element with selected colors */
     const element = document.createElement('div');
-    element.style.background = `linear-gradient(35deg, ${primaryColor}, ${secondaryColor})`;
-    
-    this.primaryColor = primaryColor;
-    this.secondaryColor = secondaryColor;
+    element.style.background = `linear-gradient(35deg, ${this.primaryColor}, ${this.secondaryColor})`;
+    element.classList.add('ball');
+
+    /* Static properties */
+    this.boundAreaEl = boundAreaEl;
     this.element = element;
+    
+    /* Dynamic properties */
+    this.boundAreaStyles = window.getComputedStyle(this.boundAreaEl);
+    this.boundAreaHeight = this.boundAreaStyles.getPropertyValue('height');
+    this.boundAreaWidth = this.boundAreaStyles.getPropertyValue('width');
+    this.styles = null;
+    this.circumference = null;
+    this.radius = null;
+    this.direction = direction;
+    this.speed = speed;
+    this.x = startX;
+    this.y = startY;
   }
 }
