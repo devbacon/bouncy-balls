9ddf54eca8c32dccf2e826876cf370af4a0fcd13
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2020-07-04 22:25:33 -0700

    refactor math utils for readability and add helper methods

--- modules/math-utils.js
+++ modules/math-utils.js
@@ -4,61 +4,53 @@ export default MathUtils = {
   /* Determine trigonometry operation based on missing side */
   findRightTriangleOppositeLength(angle, adjacentLength, hypotenuseLength) {
     /* Ensure that inputs are valid */
-    if(typeof angle != 'number' || 0 > angle > 180) return null;
-    const parsedAdjacentLength = parseInt(adjacentLength, 2);
-    const parsedHypotenuseLength = parseInt(hypotenuseLength, 2);
+    if (this.isValidTriangleAngle(angle)) return null;
 
-    if (parsedAdjacentLength) {
-      /* Tangent */
+    if (this.isValidTriangleLength(adjacentLength))
+      return adjacentLength * Math.tan(angle);
 
-      return
-    }
-
-    if (parsedHypotenuseLength) {
-      /* Sine */
-
-      return
-    }
+    if (this.isValidTriangleLength(hypotenuseLength))
+      return Math.sin(angle);
   },
+
   findRightTriangleAdjacentLength(angle, oppositeLength, hypotenuseLength) {
     /* Ensure that inputs are valid */
-    if(typeof angle != 'number' || 0 > angle > 180) return null;
-    const parsedOppositeLength = parseInt(oppositeLength, 2);
-    const parsedHypotenuseLength = parseInt(hypotenuseLength, 2);
+    if (this.isValidTriangleAngle(angle)) return null;
 
-    if (parsedOppositeLength) {
-      /* Tangent */
-      
+    if (this.isValidTriangleLength(oppositeLength))
       return
-    }
 
-    if (parsedHypotenuseLength) {
-      /* Sine */
-
+    if (this.isValidTriangleLength(hypotenuseLength))
       return
-    }
   },
+
   findRightTriangleHypotenuseLength(angle, oppositeLength, adjacentLength) {
     /* Ensure that inputs are valid */
-    if(typeof angle != 'number' || 0 > angle > 180) return null;
-    const parsedOppositeLength = parseInt(oppositeLength, 2);
-    const parsedAdjacentLength = parseInt(adjacentLength, 2);
+    if (this.isValidTriangleAngle(angle)) return null;
 
-    if (parsedOppositeLength) {
-      /* Tangent */
-      
+    if (this.isValidTriangleLength(oppositeLength))
       return
-    }
 
-    if (parsedAdjacentLength) {
-      /* Sine */
-
+    if (this.isValidTriangleLength(adjacentLength))
       return
-    }
   },
+  
   getRightTriangleDimensions(angle1, sideLength1, angle2, sideLength2, angle3, sideLength3) {
     /* Return all obtainable info using known sides and angles */
 
+  },
+
+  /* Helper methods */
+  
+  /* Validation */
+  isValidTriangleAngle(angle) {
+    if (typeof angle != 'number' || 0 > angle > 180) return false;
+    return true;
+  },
+  
+  isValidTriangleLength(number) {
+    if (typeof number != 'number' || number < 1) return false;
+    return true;
   },
 }
 
